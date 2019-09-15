153d337beab4d62d7e4d64be58da995f44411350
rattler18 <rattler18x@yandex.com>
2019-09-15 16:34:57 +0100

    Changed the Home route again

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,7 +6,7 @@ button.addEventListener('click',(event)=>{
 
 const spartan = async (input) => {
     try{
-        await fetch('https://mytodox.herokuapp.com/', {
+        await fetch('https://mytodox.herokuapp.com/todo', {
             method: 'POST',
             body: input.value
         });
@@ -27,7 +27,7 @@ list.forEach((elem)=>{
 
 const trojan = async (input) => {
     try {
-        await fetch(`https://mytodox.herokuapp.com/${input.innerText}`, {
+        await fetch(`https://mytodox.herokuapp.com/todo/${input.innerText}`, {
             method: 'DELETE',
             body: input.innerText
         });
